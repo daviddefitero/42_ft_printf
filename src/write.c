2e45c3229a4a9034748dbd5ce6e035eda1c506cb
David de Fitero Teijeira <david@defitero.com>
2025-10-25 00:52:13 +0200

    fixed signing numbers

--- src/write.c
+++ src/write.c
@@ -6,33 +6,56 @@
 /*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 22:36:15 by dde-fite          #+#    #+#             */
-/*   Updated: 2025/10/24 23:16:03 by dde-fite         ###   ########.fr       */
+/*   Updated: 2025/10/25 00:51:41 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	write_int(t_modifiers *mods, va_list *args_ptr)
+int	sign_nbr(int *nbr, t_modifiers *mods)
 {
-	int		chrs_count;
-	int		_buff_int;
+	int	chrs_count;
 
 	chrs_count = 0;
-	_buff_int = va_arg(*args_ptr, int);
-	if (mods->plus && _buff_int >= 0)
+	if (*nbr >= 0)
 	{
-		ft_putchar_fd('+', 1);
-		chrs_count++;
+		if (mods->plus)
+		{
+			ft_putchar_fd('+', 1);
+			chrs_count = 1;
+		}
+		else if (mods->blank)
+		{
+			ft_putchar_fd(' ', 1);
+			chrs_count = 1;
+		}
 	}
-	else if (mods->zero && _buff_int >= 0)
+	else if (mods->zero)
 	{
-		ft_putchar_fd(' ', 1);
-		chrs_count++;
+		ft_putchar_fd('-', 1);
+		*nbr = -*nbr;
+		chrs_count = 1;
 	}
+	return (chrs_count);
+}
+
+int	write_int(t_modifiers *mods, va_list *args_ptr)
+{
+	int				chrs_count;
+	int				nbr;
+	int				nbr_to_print;
+
+	chrs_count = 0;
+	nbr = va_arg(*args_ptr, int);
+	nbr_to_print = nbr;
+	if (mods->zero)
+		chrs_count += sign_nbr(&nbr_to_print, mods);
 	if (!mods->minus)
-		chrs_count += fill_width_start(&_buff_int, 'd', mods);
-	chrs_count += ft_nbrlen(_buff_int);
-	ft_putnbr_fd(_buff_int, 1);
+		chrs_count += fill_width_start(&nbr, 'd', mods);
+	if (!mods->zero)
+		chrs_count += sign_nbr(&nbr_to_print, mods);
+	chrs_count += ft_nbrlen(nbr);
+	ft_putnbr_fd(nbr_to_print, 1);
 	if (mods->minus)
 		chrs_count += fill_width_end(chrs_count, mods);
 	return (chrs_count);
