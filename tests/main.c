a8e09f4766ba358066ccfcfe71b2afa76133759e
David de Fitero Teijeira <david@defitero.com>
2025-10-24 21:06:31 +0200

    ampliation of tests

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 15:22:39 by dde-fite          #+#    #+#             */
-/*   Updated: 2025/10/23 22:42:15 by dde-fite         ###   ########.fr       */
+/*   Updated: 2025/10/24 20:43:21 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 int	main(void)
 {
-	ft_printf("Hola buenos dias%%\nesto es una a: %c", 'a');
+	ft_printf("Incomplete symbol: %");
+	ft_printf("\n");
+	ft_printf("Percent Symbol: %%\n");
+	ft_printf("Number %%d: %d\n", 98682);
+	ft_printf("Character %%c: %c\n", 'a');
+	ft_printf("String %%s: %s\n", "Hello World!");
 }
--- tests/original_printf.c
+++ tests/original_printf.c
@@ -6,13 +6,22 @@
 /*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 22:42:26 by dde-fite          #+#    #+#             */
-/*   Updated: 2025/10/23 22:43:36 by dde-fite         ###   ########.fr       */
+/*   Updated: 2025/10/24 21:04:24 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void)
-{
-	printf("----------ORIGINAL PRINTF----------")
-}
+// int	main(void)
+// {
+// 	printf("\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"
+// 		"\\\\\\\n");
+// 	printf(" ||||||||| ORIGINAL PRINTF |||||||||\n");
+// 	printf("///////////////////////////////////\n\n");
+// 	printf("Test 1: %#o\n", 145230);
+// 	printf("Test 1: %9d\n", 145230);
+// 	printf("Porcentaje incompleto: %");
+// 	// ft_printf("\n");
+// 	printf("Porcentaje: %%\n");
+// 	printf("Character: %c\n", 'a');
+// }
