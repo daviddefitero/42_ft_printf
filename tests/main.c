7f9d851d5e8dfbea353083ff843f594ae9c3646c
David de Fitero Teijeira <david@defitero.com>
2025-10-25 23:52:03 +0200

    more testing

--- tests/main.c
+++ tests/main.c
@@ -6,18 +6,63 @@
 /*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 15:22:39 by dde-fite          #+#    #+#             */
-/*   Updated: 2025/10/24 20:43:21 by dde-fite         ###   ########.fr       */
+/*   Updated: 2025/10/25 23:50:13 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <limits.h>
 #include "ft_printf.h"
 
 int	main(void)
 {
-	ft_printf("Incomplete symbol: %");
-	ft_printf("\n");
-	ft_printf("Percent Symbol: %%\n");
-	ft_printf("Number %%d: %d\n", 98682);
-	ft_printf("Character %%c: %c\n", 'a');
-	ft_printf("String %%s: %s\n", "Hello World!");
+	int	ret_std;
+	int	ret_ft;
+
+	printf("=== Casos básicos ===\n");
+	ret_std = printf("1. %d\n", 42);
+	ret_ft = ft_printf("1. %d\n", 42);
+	printf("-> Retornos: printf=%d, ft_printf=%d\n\n", ret_std, ret_ft);
+	ret_std = printf("2. %d\n", -42);
+	ret_ft = ft_printf("2. %d\n", -42);
+	printf("-> Retornos: printf=%d, ft_printf=%d\n\n", ret_std, ret_ft);
+	printf("=== Flags ===\n");
+	ret_std = printf("3. %+d %+d\n", 42, -42);
+	ret_ft = ft_printf("3. %+d %+d\n", 42, -42);
+	printf("-> Retornos: printf=%d, ft_printf=%d\n\n", ret_std, ret_ft);
+	ret_std = printf("4. % d % d\n", 42, -42);
+	ret_ft = ft_printf("4. % d % d\n", 42, -42);
+	printf("-> Retornos: printf=%d, ft_printf=%d\n\n", ret_std, ret_ft);
+	printf("=== Width ===\n");
+	ret_std = printf("5. %5d %5d\n", 42, -42);
+	ret_ft = ft_printf("5. %5d %5d\n", 42, -42);
+	printf("-> Retornos: printf=%d, ft_printf=%d\n\n", ret_std, ret_ft);
+	ret_std = printf("6. %-5d %-5d\n", 42, -42);
+	ret_ft = ft_printf("6. %-5d %-5d\n", 42, -42);
+	printf("-> Retornos: printf=%d, ft_printf=%d\n\n", ret_std, ret_ft);
+	ret_std = printf("7. %05d %05d\n", 42, -42);
+	ret_ft = ft_printf("7. %05d %05d\n", 42, -42);
+	printf("-> Retornos: printf=%d, ft_printf=%d\n\n", ret_std, ret_ft);
+	printf("=== Precision ===\n");
+	ret_std = printf("8. %.5d %.5d\n", 42, -42);
+	ret_ft = ft_printf("8. %.5d %.5d\n", 42, -42);
+	printf("-> Retornos: printf=%d, ft_printf=%d\n\n", ret_std, ret_ft);
+	ret_std = printf("9. %10.5d %10.5d\n", 42, -42);
+	ret_ft = ft_printf("9. %10.5d %10.5d\n", 42, -42);
+	printf("-> Retornos: printf=%d, ft_printf=%d\n\n", ret_std, ret_ft);
+	printf("=== Limites ===\n");
+	ret_std = printf("10. INT_MAX: %d\n", INT_MAX);
+	ret_ft = ft_printf("10. INT_MAX: %d\n", INT_MAX);
+	printf("-> Retornos: printf=%d, ft_printf=%d\n\n", ret_std, ret_ft);
+	ret_std = printf("11. INT_MIN: %d\n", INT_MIN);
+	ret_ft = ft_printf("11. INT_MIN: %d\n", INT_MIN);
+	printf("-> Retornos: printf=%d, ft_printf=%d\n\n", ret_std, ret_ft);
+	printf("=== Combinaciones ===\n");
+	ret_std = printf("12. %+08.5d %+08.5d\n", 42, -42);
+	ret_ft = ft_printf("12. %+08.5d %+08.5d\n", 42, -42);
+	printf("-> Retornos: printf=%d, ft_printf=%d\n\n", ret_std, ret_ft);
+	ret_std = printf("13. %-+10.3d %-+10.3d\n", 42, -42);
+	ret_ft = ft_printf("13. %-+10.3d %-+10.3d\n", 42, -42);
+	printf("-> Retornos: printf=%d, ft_printf=%d\n\n", ret_std, ret_ft);
+	return (0);
 }
--- tests/original_printf.c
+++ tests/original_printf.c
@@ -6,22 +6,24 @@
 /*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 22:42:26 by dde-fite          #+#    #+#             */
-/*   Updated: 2025/10/24 21:04:24 by dde-fite         ###   ########.fr       */
+/*   Updated: 2025/10/25 18:57:50 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int	main(void)
-// {
-// 	printf("\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"
-// 		"\\\\\\\n");
-// 	printf(" ||||||||| ORIGINAL PRINTF |||||||||\n");
-// 	printf("///////////////////////////////////\n\n");
-// 	printf("Test 1: %#o\n", 145230);
-// 	printf("Test 1: %9d\n", 145230);
-// 	printf("Porcentaje incompleto: %");
-// 	// ft_printf("\n");
-// 	printf("Porcentaje: %%\n");
-// 	printf("Character: %c\n", 'a');
-// }
+int	main(void)
+{
+	// printf("\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"
+	// 	"\\\\\\\n");
+	// printf(" ||||||||| ORIGINAL PRINTF |||||||||\n");
+	// printf("///////////////////////////////////\n\n");
+	// printf("Test 1: %#o\n", 145230);
+	// printf("Test 1: %9d\n", 145230);
+	// printf("Porcentaje incompleto: %");
+	// // ft_printf("\n");
+	// printf("Porcentaje: %%\n");
+	// printf("Character: %c\n", 'a');
+
+	printf("Number %%d: %-030.5f\n", 5064.0);
+}
